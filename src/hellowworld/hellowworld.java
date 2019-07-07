3d77eaf8c6ab34acf0af6bcd86259a0fe49345e6
1170300620 <1066815713@qq.com>
2019-07-07 21:47:35 +0800

    1170300620

--- src/hellowworld/hellowworld.java
+++ src/hellowworld/hellowworld.java
@@ -3,9 +3,9 @@ package hellowworld;
 public class hellowworld {
 
   public static void main(String[] args) {
-    // TODO 锟皆讹拷锟斤拷锟缴的凤拷锟斤拷锟斤拷锟�
     System.out.println("Hello World!");
-    System.out.println("1170300620 is ever here. 1170300620 is not here now");// useless information from 1170300620
+    System.out.println("1170300620 is ever here. 1170300620 is not here now");
+    // useless information 
   }
 
 }
