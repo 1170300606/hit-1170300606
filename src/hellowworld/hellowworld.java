1f50f1d71ed6442019e447d4a05d1f8f0bcb3527
1170300620 <1066815713@qq.com>
2019-07-07 21:49:40 +0800

    1170300620

--- src/hellowworld/hellowworld.java
+++ src/hellowworld/hellowworld.java
@@ -3,9 +3,10 @@ package hellowworld;
 public class hellowworld {
 
   public static void main(String[] args) {
+    // TODO ×Ô¶¯Éú³ÉµÄ·½·¨´æ¸ù
     System.out.println("Hello World!");
-    System.out.println("1170300620 is ever here. 1170300620 is not here now");
-    // useless information 
+    System.out.println("1170300620 is ever here.");
+    System.out.println("1170300604 is ever here too.");
   }
 
 }
